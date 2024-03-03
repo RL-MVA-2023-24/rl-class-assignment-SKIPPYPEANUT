00254cc19e69ff5a0c592b9f3269482d0f502b9f
Peanut <47550770+Yiling-Z@users.noreply.github.com>
2024-03-03 17:52:05 +0100

    Add files via upload
    test, dqn train for 10 epochs

--- src/train.py
+++ src/train.py
@@ -1,5 +1,9 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
+import torch
+import numpy as np
+import random
+import torch.nn as nn
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -10,12 +14,157 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
+
+class ReplayBuffer:
+    def __init__(self, capacity, device):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+        self.device = device
+    def append(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        return list(map(lambda x:torch.Tensor(np.array(x)).to(self.device), list(zip(*batch))))
+    def __len__(self):
+        return len(self.buffer)
+
+def greedy_action(network, state):
+    device = "cuda" if next(network.parameters()).is_cuda else "cpu"
+    with torch.no_grad():
+        Q = network(torch.Tensor(state).unsqueeze(0).to(device))
+        return torch.argmax(Q).item()
+class DQN(nn.Module):
+    def __init__(self, state_dim, nb_actions):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, nb_actions)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+        self.batchnorm1 = nn.BatchNorm1d(128)
+        self.batchnorm2 = nn.BatchNorm1d(256)
+        self.batchnorm3 = nn.BatchNorm1d(128)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.batchnorm1(x)
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.relu(self.fc3(x))
+        x = self.batchnorm3(x)
+        x = self.dropout(x)
+        x = self.fc4(x)
+        return x
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+state_dim = env.observation_space.shape[0]
+nb_actions= env.action_space.n
+nb_neurons= 256
+
+config = {'nb_actions': nb_actions,
+          'learning_rate': 0.001,
+          'gamma': 0.95,
+          'buffer_size': 1000000,
+          'epsilon_min': 0.01,
+          'epsilon_max': 1.,
+          'epsilon_decay_period': 10000,
+          'epsilon_delay_decay': 2000,
+          'batch_size': 256}
+model = DQN(state_dim,nb_actions)         
 class ProjectAgent:
-    def act(self, observation, use_random=False):
-        return 0
+    def __init__(self):
+        device = "cuda" if next(model.parameters()).is_cuda else "cpu"
+        self.gamma = config['gamma']
+        self.batch_size = config['batch_size']
+        self.nb_actions = config['nb_actions']
+        self.memory = ReplayBuffer(config['buffer_size'], device)
+        self.epsilon_max = config['epsilon_max']
+        self.epsilon_min = config['epsilon_min']
+        self.epsilon_stop = config['epsilon_decay_period']
+        self.epsilon_delay = config['epsilon_delay_decay']
+        self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
+        self.model = model 
+        self.criterion = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config['learning_rate'])
+    
+    def gradient_step(self):
+        if len(self.memory) > self.batch_size:
+            X, A, R, Y, D = self.memory.sample(self.batch_size)
+            QYmax = self.model(Y).max(1)[0].detach()
+            #update = torch.addcmul(R, self.gamma, 1-D, QYmax)
+            update = torch.addcmul(R, 1-D, QYmax, value=self.gamma)
+            QXA = self.model(X).gather(1, A.to(torch.long).unsqueeze(1))
+            loss = self.criterion(QXA, update.unsqueeze(1))
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step() 
+    
+    def train(self, env, max_episode):
+        episode_return = []
+        episode = 0
+        episode_cum_reward = 0
+        state, _ = env.reset()
+        epsilon = self.epsilon_max
+        step = 0
 
+        while episode < max_episode:
+            # update epsilon
+            if step > self.epsilon_delay:
+                epsilon = max(self.epsilon_min, epsilon-self.epsilon_step)
+
+            # select epsilon-greedy action
+            if np.random.rand() < epsilon:
+                action = env.action_space.sample()
+            else:
+                action = greedy_action(self.model, state)
+
+            # step
+            next_state, reward, done, trunc, _ = env.step(action)
+            self.memory.append(state, action, reward, next_state, done)
+            episode_cum_reward += reward
+
+            # train
+            self.gradient_step()
+
+            # next transition
+            step += 1
+            if done | trunc:
+                episode += 1
+                print("Episode ", '{:3d}'.format(episode), 
+                      ", epsilon ", '{:6.2f}'.format(epsilon), 
+                      ", batch size ", '{:5d}'.format(len(self.memory)), 
+                      ", episode return ", '{:4.1f}'.format(episode_cum_reward),
+                      sep='')
+                state, _ = env.reset()
+                episode_return.append(episode_cum_reward)
+                episode_cum_reward = 0
+            else:
+                state = next_state
+
+        return episode_return
+    def act(self, state):
+      return greedy_action(self.model, state)   
     def save(self, path):
-        pass
-
+      torch.save(self.model.state_dict(), path)
     def load(self):
-        pass
+        self.model.load_state_dict(torch.load('dqn_agent_10.pth', map_location=device))
+        self.model.eval()
+
+
+
+
+if __name__ == "__main__":
+  pass
+    # model = DQN(state_dim,nb_actions)
+    # agent = ProjectAgent(config,model)
+    # print(config)
+    # max_episod = 10
+    # episode_return = agent.train(env, max_episod)
+    # agent.save(f'dqn_agent_{max_episod}.pth')
